cd44d05ed5b52f59042746364cdb8242384dcf7c
nadunnr <naveendra0729@gmail.com>
2024-03-19 02:13:30 +0530

    full dir code

--- tif_to_nifti.py
+++ tif_to_nifti.py
@@ -10,6 +10,7 @@ import numpy as np
 input_path = "D:/VIP Cup/Dataset/ICIP training data/0/RawDataQA (1)/"
 output_path= "D:/VIP Cup/test12.nii.gz"
 vip_dataset = "D:/VIP Cup/Dataset/ICIP training data/"
+output_dataset = "D:/VIP Cup/ICIP data"
 
 converted_nifti_count = 0
 
@@ -36,6 +37,12 @@ def convert_tiff_dir_to_nifti(input_dir, output_path, stack_axis=2):
     except Exception as e:
         print(e)
 
+def out_path(in_path):
+    base_name = os.path.basename(in_path)
+    class_name = os.path.basename(os.path.dirname(in_path))
+    out_path = os.path.join(output_dataset,class_name) + "/" + base_name + ".nii.gz"
+    return out_path
+
 convert_tiff_dir_to_nifti(input_path, output_path)
 
 if __name__ == "__main__":
@@ -44,4 +51,16 @@ if __name__ == "__main__":
 
     for i,file in enumerate(dataset):
         if is_tiff_dir(file):
-            print(file)
+
+            # output directory of the corresponding nifti file
+            dest_file = out_path(file)
+            new_dir = os.path.dirname(dest_file)
+            os.makedirs(new_dir, exist_ok=True)
+
+            #converting tiff directory to nifti
+            try:
+                convert_tiff_dir_to_nifti(file, dest_file, stack_axis=2)
+                converted_nifti_count += 1
+                print(f"Succesfully converted total files: {converted_nifti_count}")
+            except Exception as e:
+                print(e)
