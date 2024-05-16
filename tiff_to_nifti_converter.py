6c12572f5f5300ec10c15060372b5b98c3690c57
nadunnr <naveendra0729@gmail.com>
2024-05-16 22:01:56 +0530

    fix sorting error in 2d layers

--- tiff_to_nifti_converter.py
+++ tiff_to_nifti_converter.py
@@ -5,8 +5,8 @@ from PIL import Image
 import nibabel as nib
 import numpy as np
 
-input_path = "D:/VIP Cup/Dataset/ICIP training data/0/RawDataQA (1)/"
-output_path= "D:/VIP Cup/test12.nii.gz"
+input_path = "D:/VIP Cup/Dataset/ICIP training data/0/RawDataQA (32)/"
+output_path= "D:/VIP Cup/test_0_32.nii.gz"
 vip_dataset = "D:/VIP Cup/Dataset/ICIP training data/"
 output_dataset = "D:/VIP Cup/ICIP data"
 
@@ -18,20 +18,33 @@ def is_tiff_dir(curr_path):
     return num_dcm>0
 
 def convert_tiff_dir_to_nifti(input_dir, output_path, stack_axis=2):
-    '''function to convert tiff dir to nifti files'''
+    '''
+        function to convert tiff dir to nifti files
+        stacking on z axis, stack_axis=2
+    '''
     try:
         img_dir = input_dir
-        fns = sorted([str(fn) for fn in glob.glob(os.path.join(img_dir,'*.tiff*'))])
-        if not fns:
+        fns = list([str(oct_image) for oct_image in glob.glob(os.path.join(img_dir,'*.tiff*'))])
+
+        case_no = lambda x: int(x.split('(')[-1].split(')')[0])
+        oct_images = sorted(fns, key = lambda y: case_no(y))
+
+
+        if not oct_images:
             raise ValueError(f'img_dir ({input_dir}) does not contain any .tif or .tiff images.')
+        
         imgs = []
-        for fn in fns:
-            img = np.asarray(Image.open(fn)).astype(np.float32).squeeze()
+        for oct_image in oct_images:
+            #print(oct_image)
+            
+            img = np.asarray(Image.open(oct_image)).astype(np.float32).squeeze()
             if img.ndim != 2:
-                raise Exception(f'Only 2D data supported. File {fn} has dimension {img.ndim}.')
+                raise Exception(f'Only 2D data supported. File {oct_image} has dimension {img.ndim}.')
             imgs.append(img)
+
         img = np.stack(imgs, stack_axis)
         nib.Nifti1Image(img,None).to_filename(output_path)
+
     except Exception as e:
         print(e)
 
@@ -42,7 +55,7 @@ def out_path(in_path):
     out_path = os.path.join(output_dataset,class_name) + "/" + base_name + ".nii.gz"
     return out_path
 
-convert_tiff_dir_to_nifti(input_path, output_path)
+#convert_tiff_dir_to_nifti(input_path, output_path)
 
 if __name__ == "__main__":
 
@@ -61,5 +74,6 @@ if __name__ == "__main__":
                 convert_tiff_dir_to_nifti(file, dest_file, stack_axis=2)
                 converted_nifti_count += 1
                 print(f"Succesfully converted total files: {converted_nifti_count}")
+
             except Exception as e:
                 print(e)
